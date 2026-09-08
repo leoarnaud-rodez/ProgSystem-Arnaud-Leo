5eed91ab33efd5e4a0f8c8e294563c33d686f380
LEO ARNAUD <leo.arnaud@iut.local>
2026-09-08 16:11:36 +0200

    avancée du TP1 : création de FirstPPm.java et Image.java

---
+++ TP1_LectureEcritureJava/Image.java
@@ -0,0 +1,47 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Image {
+    private int width;
+    private int height;
+    // pixels[y][x][0=R,1=G,2=B]
+    private int[][][] pixels; // pixels[y][x][0=R,1=G,2=B]
+
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+
+    /**
+     * Constructeur : initialise une image vide.
+     */
+    public Image(int width, int hauteur) {
+        this.width = width;
+        this.height = height;
+        pixels = new int[width][height][3];
+    }
+
+    /**
+     * Définit la couleur d'un pixel à la position (x, y)
+     */
+    public void setPixel(int x, int y, int r, int g, int b) {
+        if (x >= 0 && x < height && y >= 0 && y < width) {
+            pixels[y][x][0] = r;
+            pixels[y][x][1] = g;
+            pixels[y][x][2] = b;
+        }
+    }
+
+    /**
+     * Sauvegarde l'image au format texte PPM (P3)
+     */
+    public void save_txt(String filename) throws IOException {
+        FileWriter writer = new FileWriter("Image.ppm");
+        
+        writer.write("P3\n");
+
+        // TODO écrire les composants de l'image (dim, pixels, autre...)
+
+        writer.close();
+
+        System.out.println("Image PPM créée sans accros !");
+    }
+}
