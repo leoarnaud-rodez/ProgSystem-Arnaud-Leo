212083796fdf3eff7098b70a07806edf8c073e52
LEO ARNAUD <leo.arnaud@iut.local>
2026-09-15 16:41:05 +0200

    ajout de la méthode save_bin (TODO) et de la question sur les write

--- TP1_LectureEcritureJava/Image.java
+++ TP1_LectureEcritureJava/Image.java
@@ -1,9 +1,10 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileOutputStream;
 
 public class Image {
     private int width;
-    private int height;    // pixels[y][x][0=R,1=G,2=B]
+    private int height;    	  // pixels[y][x][0=R,1=G,2=B]
     private int[][][] pixels; // pixels[y][x][0=R,1=G,2=B]
 
     public int getWidth() { return width; }
@@ -38,6 +39,29 @@ public class Image {
         writer.write("P3\n");
         writer.write(width + " " + height + "\n");
         writer.write("255\n");
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                writer.write(pixels[y][x][0] + " " + pixels[y][x][1] + " " + pixels[y][x][2] + " ");
+            }
+            writer.write("\n");
+        }
+
+        writer.close();
+    }
+	
+	/**
+     * Sauvegarde l'image au format binaire (P6)
+	 *
+	 * utiliser write(byte[] pixels = new byte[3]) et FileOutputStream
+	 * besoin de rien d'autre
+     */
+    public void save_bin(String filename) throws IOException {
+        FileOutputStream writer = new FileOutputStream(filename);
+
+        writer.write("P6\n");
+        writer.write(width + " " + height + "\n");
+        writer.write("255\n");    // a mettre sur une seule case
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
