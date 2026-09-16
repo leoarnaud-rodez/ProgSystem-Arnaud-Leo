caa950960113c89b8cdfbeb4314f04cc9340853d
LEO ARNAUD <leo.arnaud@iut.local>
2026-09-16 11:57:34 +0200

    très légère avancée sur Utils.java

--- TP2/utils.java
+++ TP2/utils.java
@@ -1,8 +1,19 @@
+/*
+ * Utils.java                                          16 sept. 2026
+ * IUT de Rodez, pas de copyright (ni de "copyleft")
+ */
+
+
 public class Utils {
 
     public static int writeInt(byte[] memory, int offset, int value) {
         // TODO: Écrire les 4 octets de 'value' dans 'memory'
         // à partir de 'offset', en big-endian.
+		
+		// piste approximative : byte b3 = (byte)(value0xFF)
+		// faire avec les décalages >> ou <<
+		// memory[offset] = b3
+		
         return 4;
     }
 
